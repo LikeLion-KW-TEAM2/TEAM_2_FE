cb7b86db5950817dea4f3281dbf9cb0019343cc9
김동균 <wooheuy5752@nate.com>
2024-07-13 14:40:29 +0900

    add : tailwind 설치 및 eslint, prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,44 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-imports-ts': ['error'],
+    'unused-imports/no-unused-vars': [
       'warn',
-      { allowConstantExport: true },
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
     ],
+    'no-duplicate-imports': 'off',
+    'import/newline-after-import': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '_',
+        varsIgnorePattern: '_',
+      },
+    ],
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          ['^react', '^@?\\w'],
+          ['@/(.*)'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'warn',
+    'prettier/prettier': 'error',
   },
 }
