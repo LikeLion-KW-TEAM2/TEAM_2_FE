aa6fee5772492fd61cd5fe52a96fa9ffb34cd6b6
김동균 <wooheuy5752@nate.com>
2024-07-13 15:07:02 +0900

    feat : 서버 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useMutation } from '@tanstack/react-query'
+import instance from './services/@core'
+
 function App() {
+  const postData = useMutation({
+    mutationFn: () => instance.post(`/api/test`, { name: 'kim', age: 12 }),
+    onSuccess: (res) => {
+      console.log('post 성공', res)
+      alert('post 성공')
+    },
+    onError: (error) => {
+      console.log('post 실패', error)
+      alert('post 실패')
+    },
+  })
   return (
     <>
       <div className="w-8 text-lg font-bold">hello</div>
+      <button onClick={() => postData.mutate()}>post 요청</button>
     </>
   )
 }
