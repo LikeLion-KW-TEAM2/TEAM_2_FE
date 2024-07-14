d70d183935f49e7c26cf48c9187fd946bd063041
김동균 <wooheuy5752@nate.com>
2024-07-14 20:48:03 +0900

    style 전체 컨테이너 색상 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import BottomNav from './components/BottomNav'
 function App() {
   return (
     <div className="flexCenter">
-      <div className="h-svh w-full min-w-[320px] max-w-[450px] bg-slate-300">
+      <div className="h-svh w-full min-w-[320px] max-w-[450px] border-x">
         <BottomNav />
       </div>
     </div>
