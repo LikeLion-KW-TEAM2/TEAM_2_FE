79a35a12e22c775f60f8cb3492c1dea24585b7cf
김동균 <wooheuy5752@nate.com>
2024-07-14 20:47:25 +0900

    feat : 하단 nav바 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import { useMutation } from '@tanstack/react-query'
-import instance from './services/@core'
+import BottomNav from './components/BottomNav'
 
 function App() {
-  const postData = useMutation({
-    mutationFn: () => instance.post(`/api/test`, { name: 'kim', age: 12 }),
-    onSuccess: (res) => {
-      console.log('post 성공', res)
-      alert('post 성공')
-    },
-    onError: (error) => {
-      console.log('post 실패', error)
-      alert('post 실패')
-    },
-  })
   return (
     <div className="flexCenter">
       <div className="h-svh w-full min-w-[320px] max-w-[450px] bg-slate-300">
-        <div className="w-8 text-lg font-bold">hello</div>
-        <button onClick={() => postData.mutate()}>post 요청</button>
-        <p>stop</p>
+        <BottomNav />
       </div>
     </div>
   )
---
+++ src/components/BottomNav.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react'
+import { TbPencilCheck } from 'react-icons/tb'
+import { PiIceCreamBold, PiUsersThreeBold } from 'react-icons/pi'
+import { BiBookHeart } from 'react-icons/bi'
+
+const NAV_ITEMS = [
+  { name: '기록', icon: <TbPencilCheck size={24} /> },
+  { name: '아이스크림', icon: <PiIceCreamBold size={24} /> },
+  { name: '친구', icon: <PiUsersThreeBold size={24} /> },
+  { name: '방명록', icon: <BiBookHeart size={24} /> },
+]
+
+interface Item {
+  item: {
+    name: string
+    icon: ReactNode
+  }
+}
+
+const NavItem = ({ item }: Item) => {
+  return (
+    <div className="flexColumnAlign text-primary-400">
+      {item.icon}
+      <p className="text-xsmall">{item.name}</p>
+    </div>
+  )
+}
+
+const BottomNav = () => {
+  return (
+    <div className="flexAlign justify-evenly py-5">
+      {NAV_ITEMS.map((item) => (
+        <NavItem key={item.name} item={item} />
+      ))}
+    </div>
+  )
+}
+
+export default BottomNav
