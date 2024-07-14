420faa59afff9c415497e560a2f0ee0f575712c7
김동균 <wooheuy5752@nate.com>
2024-07-14 23:55:40 +0900

    feat : button props 처리

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,34 @@
 import { ReactNode } from 'react'
 
 interface IButton {
+  type?: 'button' | 'submit' | 'reset' | undefined
   size: string
-  width: string
+  width?: string
   children: ReactNode
   disabled?: boolean
+  handleClick?: () => void
 }
 
-const Button = ({ size, width, children, disabled = false }: IButton) => {
+const Button = ({
+  type = 'button',
+  size,
+  width = 'w-fit',
+  children,
+  disabled = false,
+  handleClick,
+}: IButton) => {
   let style = ''
-  if (size === 'large') style = 'px-8 font-large'
-  else if (size === 'medium') style = 'px-8 font-medium'
-  else if (size === 'small') style = 'px-8 font-small'
-  else if (size === 'xsmall') style = 'px-8 font-xsmall'
+  if (size === 'large') style = 'px-8 text-large'
+  else if (size === 'medium') style = 'px-8 text-medium'
+  else if (size === 'small') style = 'px-5 text-small'
+  else if (size === 'xsmall') style = 'px-4 text-xsmall'
 
   return (
     <button
-      className={`text-secondary-50 shadow-large bg-primary-600 rounded-full py-3 font-bold transition-all hover:scale-[1.02] ${width} ${style} ${disabled && 'bg-secondary-50'}`}
+      type={type}
+      className={`text-secondary-50 shadow-large bg-primary-600 flex-shrink-0 rounded-full py-3 font-bold transition-all hover:scale-[1.02] ${width} ${style} ${disabled && 'bg-secondary-50'}`}
       disabled={disabled}
+      onClick={handleClick}
     >
       {children}
     </button>
