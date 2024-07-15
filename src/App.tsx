8a3aaec28200a12f9a0a2891ad36661905952b05
김동균 <wooheuy5752@nate.com>
2024-07-15 16:03:09 +0900

    feat : 회원가입 모달 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Router from './components/Router'
 function App() {
   return (
     <div className="flexCenter">
-      <div className="scrollbar-hide h-svh w-full min-w-[320px] max-w-[450px] overflow-scroll border-x px-4">
+      <div className="scrollbar-hide relative h-svh w-full min-w-[320px] max-w-[450px] overflow-scroll border-x px-4">
         <Router />
       </div>
     </div>
--- src/pages/auth/components/SignupCheckTab.tsx
+++ src/pages/auth/components/SignupCheckTab.tsx
@@ -1,6 +1,9 @@
 import CheckInput from './CheckInput'
 import { BsCheckSquareFill } from 'react-icons/bs'
 import useCheckClick from '@/hooks/useCheckClick'
+import Button from '@/components/Button'
+import { Modal } from './modal/Modal'
+import { useModal } from '@/hooks/useModal'
 
 const AGREE_LIST = [
   '사용자 이용 약관 동의',
@@ -9,31 +12,48 @@ const AGREE_LIST = [
 ]
 
 const SignupCheckTab = () => {
+  const { isOpen, openModal, closeModal } = useModal()
   const { isChecked, isAllChecked, handleCheckClick, handleAllCheckClick } =
     useCheckClick()
 
+  const handleFinishForm = () => {
+    openModal()
+  }
+
   return (
-    <div className="text-secondary-900">
-      <div className="flexAlign mb-8 gap-2" onClick={handleAllCheckClick}>
-        <BsCheckSquareFill
-          size={24}
-          className={`${isAllChecked ? 'text-primary-600' : 'text-secondary-400'}`}
-        />
-        <h5 className="font-bold">모두 동의합니다.</h5>
+    <>
+      <div className="text-secondary-900">
+        <div className="flexAlign mb-8 gap-2" onClick={handleAllCheckClick}>
+          <BsCheckSquareFill
+            size={24}
+            className={`${isAllChecked ? 'text-primary-600' : 'text-secondary-400'}`}
+          />
+          <h5 className="font-bold">모두 동의합니다.</h5>
+        </div>
+
+        <div className="flexColumn gap-5">
+          {AGREE_LIST.map((item, index) => (
+            <CheckInput
+              key={index}
+              isChecked={isChecked[index]}
+              handleClick={() => handleCheckClick(index)}
+            >
+              {item}
+            </CheckInput>
+          ))}
+        </div>
+
+        <div className="absolute bottom-10 left-4 right-4">
+          <Button width="w-full" size="large" handleClick={handleFinishForm}>
+            회원가입 완료
+          </Button>
+        </div>
       </div>
 
-      <div className="flexColumn gap-5">
-        {AGREE_LIST.map((item, index) => (
-          <CheckInput
-            key={index}
-            isChecked={isChecked[index]}
-            handleClick={() => handleCheckClick(index)}
-          >
-            {item}
-          </CheckInput>
-        ))}
-      </div>
-    </div>
+      <Modal isOpen={isOpen} closeModal={closeModal}>
+        <div>hi</div>
+      </Modal>
+    </>
   )
 }
 
