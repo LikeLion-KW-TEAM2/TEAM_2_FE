c35cf09f930d6001f4c3de16a1fc932163d10a1a
김동균 <wooheuy5752@nate.com>
2024-07-17 16:49:53 +0900

    chore : 친구페이지 입력창 수정

--- src/pages/friend/components/SearchInput.tsx
+++ src/pages/friend/components/SearchInput.tsx
@@ -2,10 +2,10 @@ import { RiSearchLine } from 'react-icons/ri'
 
 const SearchInput = () => {
   return (
-    <div className="flexAlign gap-3 rounded-xl p-2 shadow-medium">
+    <div className="flexAlign gap-3 rounded-xl p-2 shadow-md">
       <RiSearchLine size={24} className="text-secondary-400" />
       <input
-        className="text-secondary-900 focus:outline-none"
+        className="flex-1 text-secondary-900 focus:outline-none"
         placeholder="친구의 아이디를 입력해주세요"
       />
     </div>
