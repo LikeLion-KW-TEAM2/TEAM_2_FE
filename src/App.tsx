96b3a6396aaca46fedaf7125c8330cdd13079edc
김동균 <wooheuy5752@nate.com>
2024-07-17 22:50:43 +0900

    feat : 스플래시 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react'
 import Router from './components/Router'
+import Splash from './components/Splash'
 
 function App() {
+  const [showSplash, setShowSplash] = useState<boolean>(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="flexCenter font-sans">
       <div className="relative h-svh w-full min-w-[320px] max-w-[450px] overflow-scroll border-x px-4 scrollbar-hide">
-        <Router />
+        {showSplash ? <Splash /> : <Router />}
       </div>
     </div>
   )
---
+++ src/components/Splash.tsx
@@ -0,0 +1,14 @@
+const Splash = () => {
+  return (
+    <div className="grid h-full w-full place-items-center">
+      <div className="flexColumnAlign gap-8">
+        <h1 className="text-[55px] font-bold text-primary-900">돈두댓</h1>
+        <p className="text-medium text-primary-600">
+          하지말아야 할 습관을 기록하자
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Splash
