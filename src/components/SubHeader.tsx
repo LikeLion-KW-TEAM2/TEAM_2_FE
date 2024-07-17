a8ea29aa3e680ad8f51008b038b1c474542dc860
김동균 <wooheuy5752@nate.com>
2024-07-17 16:04:22 +0900

    feat : subheader 뒤로 가기 설정

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import { AiOutlineLeft } from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom'
 
 interface ISubHeader {
   hidden?: boolean
@@ -7,11 +8,14 @@ interface ISubHeader {
 }
 
 const SubHeader = ({ hidden = false, children }: ISubHeader) => {
+  const navigate = useNavigate()
+
   return (
     <div className="flexCenter relative mb-3 p-4">
       <AiOutlineLeft
         size={20}
         className="absolute left-4 top-5 text-secondary-400"
+        onClick={() => navigate(-1)}
       />
       <h5 className="font-bold text-primary-900">{children}</h5>
       <p className="absolute right-4 top-5 text-medium font-medium text-error-primary">
