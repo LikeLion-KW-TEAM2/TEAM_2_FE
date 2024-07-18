3143458bd1b1eb05f2cff2e20699be63921a3a5b
김동균 <wooheuy5752@nate.com>
2024-07-18 15:43:20 +0900

    feat : 마이페이지 링크 연결

--- src/components/HeaderWithProfile.tsx
+++ src/components/HeaderWithProfile.tsx
@@ -1,14 +1,17 @@
 import defaultImg from '@/assets/images/default.svg'
+import { Link } from 'react-router-dom'
 
 const HeaderWithProfile = () => {
   return (
     <div className="flexBetweenAlign mb-10 mt-6">
       <h1 className="font-bold text-primary-900">돈두댓</h1>
-      <img
-        src={defaultImg}
-        className="h-14 w-14 rounded-full border-[0.5px] border-secondary-200 bg-secondary-50 object-cover"
-        alt="default-img"
-      />
+      <Link to={'/mypage'}>
+        <img
+          src={defaultImg}
+          className="h-14 w-14 rounded-full border-[0.5px] border-secondary-200 bg-secondary-50 object-cover"
+          alt="default-img"
+        />
+      </Link>
     </div>
   )
 }
--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -14,11 +14,11 @@ const SubHeader = ({ hidden = false, children }: ISubHeader) => {
     <div className="flexCenter relative mb-3 p-4">
       <AiOutlineLeft
         size={20}
-        className="absolute left-4 top-5 text-secondary-400"
+        className="absolute left-4 top-5 cursor-pointer text-secondary-400"
         onClick={() => navigate(-1)}
       />
       <h5 className="font-bold text-primary-900">{children}</h5>
-      <p className="absolute right-4 top-5 text-medium font-medium text-error-primary">
+      <p className="absolute right-4 top-5 cursor-pointer text-medium font-medium text-error-primary">
         {!hidden && '삭제'}
       </p>
     </div>
