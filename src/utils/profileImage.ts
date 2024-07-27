28c5e861d874a7c99ce358b8221e0ed6182dab45
김동균 <wooheuy5752@nate.com>
2024-07-27 16:54:56 +0900

    del : 안쓰는 consol.log 제거

--- src/utils/profileImage.ts
+++ src/utils/profileImage.ts
@@ -1,7 +1,6 @@
 import defaultImg from '@/assets/images/default.svg'
 
 const profileImage = (image?: string) => {
-  console.log(image)
   if (image && image !== 'default') return image
   return defaultImg
 }
