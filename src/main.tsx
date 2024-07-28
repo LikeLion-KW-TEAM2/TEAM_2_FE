fd40e8d99d9d1fbcf3b31523ba4bc43c09c3a1ef
김동균 <wooheuy5752@nate.com>
2024-07-28 15:35:32 +0900

    del : 리액트 쿼리 데브툴스 제거

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +30,6 @@ enableMocking().then(() => {
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>,
   )
