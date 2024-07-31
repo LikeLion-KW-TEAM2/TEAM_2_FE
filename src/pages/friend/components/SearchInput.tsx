b424c352be1d95ad4997a7d46f69e23245eebc21
김동균 <wooheuy5752@nate.com>
2024-07-31 15:26:03 +0900

    chore : 검색버튼 아이폰 오류 수정

--- src/pages/friend/components/SearchInput.tsx
+++ src/pages/friend/components/SearchInput.tsx
@@ -13,7 +13,7 @@ const SearchInput = () => {
   }
 
   return (
-    <div className="flexAlign mb-8 gap-3 rounded-xl px-4 py-2 shadow-md">
+    <div className="flexAlign mb-8 gap-3 overflow-hidden rounded-xl px-4 py-2 shadow-md">
       <input
         type="search"
         name="searchId"
@@ -33,7 +33,7 @@ const SearchInput = () => {
       )}
       <RiSearchLine
         size={24}
-        className="cursor-pointer text-secondary-400"
+        className="flex-shrink-0 cursor-pointer text-secondary-400"
         onClick={handleClickSearch}
       />
     </div>
